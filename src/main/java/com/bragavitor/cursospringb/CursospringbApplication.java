3b2ad0e3ec3585983f51e676b2c5c37651d76c4c
Vitor Braga <joaovitor1702@gmail.com>
2021-08-11 21:03:32 -0300

    Projeto Criado

---
+++ src/main/java/com/bragavitor/cursospringb/CursospringbApplication.java
@@ -0,0 +1,13 @@
+package com.bragavitor.cursospringb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bragavitor/cursospringb/CursospringbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bragavitor.cursospringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
