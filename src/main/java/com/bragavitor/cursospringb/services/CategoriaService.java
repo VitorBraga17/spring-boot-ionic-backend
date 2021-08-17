eebd59c6620047922e59f469cecbbbc6846a9156
Vitor Braga <joaovitor1702@gmail.com>
2021-08-17 19:42:47 -0300

    Ajustes no endpoint

--- src/main/java/com/bragavitor/cursospringb/services/CategoriaService.java
+++ src/main/java/com/bragavitor/cursospringb/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.bragavitor.cursospringb.domain.Categoria;
 import com.bragavitor.cursospringb.repositories.CategoriaRepository;
+import com.bragavitor.cursospringb.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,9 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id) {
+    public Categoria find(Integer id) { 
         Optional<Categoria> obj = repo.findById(id); 
-        return obj.orElse(null); 
-    }
-
+       return obj.orElseThrow(() -> new ObjectNotFoundException( 
+        "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+       } 
 }
