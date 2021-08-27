8c3fbc902814141131bbdf4b4d8f03d5a96966be
Vitor Braga <joaovitor1702@gmail.com>
2021-08-26 21:11:30 -0300

    PUT,DELETE e GET para cliente

---
+++ src/main/java/com/bragavitor/cursospringb/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package com.bragavitor.cursospringb.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.bragavitor.cursospringb.domain.Cliente;
+
+import org.hibernate.validator.constraints.Length;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Length(min = 5, max = 120 , message = "O tamanho deve ser entre 5 e 120 caracteres")
+    private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Email(message = "Email Invalido")
+    private String email;
+
+
+    public ClienteDTO(){
+
+    }
+
+    public ClienteDTO(Cliente obj){
+        id = obj.getId();
+        nome = obj.getNome();
+        email = obj.getEmail();
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+
+}
