f02bb347058cf1892aa138c68d3806d0350681b3
Vitor Braga <joaovitor1702@gmail.com>
2021-08-31 22:52:30 -0300

    validacao de email na insercao de cliente

--- src/main/java/com/bragavitor/cursospringb/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/bragavitor/cursospringb/services/validation/ClienteInsertValidator.java
@@ -5,12 +5,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.bragavitor.cursospringb.domain.Cliente;
 import com.bragavitor.cursospringb.domain.enums.TipoCliente;
 import com.bragavitor.cursospringb.dto.ClienteNewDTO;
+import com.bragavitor.cursospringb.repositories.ClienteRepository;
 import com.bragavitor.cursospringb.resources.exceptions.FieldMessage;
 import com.bragavitor.cursospringb.services.validation.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+ @Autowired
+ private ClienteRepository repo;
+
  @Override
  public void initialize(ClienteInsert ann) {
  }
@@ -25,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
  if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
     list.add(new FieldMessage("cpfOuCnpj","CNPJ Invalido"));
 }
+
+Cliente aux = repo.findByEmail(objDto.getEmail());
+if(aux != null){
+    list.add(new FieldMessage("email","Email ja existente"));
+}
+
  
 for (FieldMessage e : list) {
  context.disableDefaultConstraintViolation();
