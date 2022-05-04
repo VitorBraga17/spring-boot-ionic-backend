1b70fa58371418b8cae60ef803fba55d4b5f91e9
Vitor Braga <joaovitor1702@gmail.com>
2022-05-04 11:29:43 -0300

    Criando Profile de Desenvolvimento

---
+++ src/main/java/com/bragavitor/cursospringb/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.bragavitor.cursospringb.config;
+
+import com.bragavitor.cursospringb.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public Boolean instantiateDatabase() throws ParseException {
+        if(!"create".equals(strategy))// garantir que o banco seja alimentado somente uma vez
+            return false;
+
+        dbService.instantiateTestDatabase();
+        return true;
+
+    }
+}
