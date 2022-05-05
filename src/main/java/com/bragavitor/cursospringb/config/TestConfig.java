6ccc126a00a61429222bb9828633ba0c0a323f62
Vitor Braga <joaovitor1702@gmail.com>
2022-05-05 17:14:32 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/bragavitor/cursospringb/config/TestConfig.java
+++ src/main/java/com/bragavitor/cursospringb/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.bragavitor.cursospringb.config;
 
 import com.bragavitor.cursospringb.services.DBService;
+import com.bragavitor.cursospringb.services.EmailService;
+import com.bragavitor.cursospringb.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +22,10 @@ public class TestConfig {
         dbService.instantiateTestDatabase();
 
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new MockEmailService();
     }
 }
